af406df1af076a9df64628432426bfe84d44e085
pelletierj97 <pelletierj97@gmail.com>
2016-10-26 13:19:31 -0400

    HTML parser and tasks laid out

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-// This is where the main function will be executed.
+#! python3
+
+# This is where the main function will be executed.
+import webbrowser, requests, os, bs4
+
+def downloadComic(url):
+    os.makedirsnal(finalComic, exists_ok = True) # The exists_ok = True prevents the program from giving us an exception when the folder exists
+    while not url.endswith('#'):
+        # TODO: Download the page
+        print("Downloading page {}".format(url))
+
+        html = requests.get(url)
+        html.raise_for_status() # Raises an HTTPerror if something went wrong with the request
+        parsedHTML = BeautifulSoup(html)
+        # TODO: Find the URL of the comic page
+
+        # TODO: Download the image
+
+        # TODO: Save the image to ./finalComic
+
+    print("Done.")
