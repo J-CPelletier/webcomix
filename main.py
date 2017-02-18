d66ec29e1a7160536a639777648b046d6dbcdb79
Jean-Christophe Pelletier <pelletierj97@gmail.com>
2017-02-18 12:01:49 -0500

    Add Guilded Age to the list of supported comics

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ supported_comics = {
     "JL8": ("http://limbero.org/jl8/1", "//b[2]/a/@href", "//img/@src"),
     "SMBC": ("http://www.smbc-comics.com/comic/2002-09-05", "//a[@class='next']/@href", "//img[@id='cc-comic']/@src"),
     "Blindsprings": ("http://www.blindsprings.com/comic/blindsprings-cover-book-one", "//a[@class='next']/@href", "//img[@id='cc-comic']/@src"),
-    "TheAbominableCharlesChristopher": ("http://abominable.cc/post/44164796353/episode-one", "//span[@class='next_post']//@href", "//div[@class='photo']//img/@src")
+    "TheAbominableCharlesChristopher": ("http://abominable.cc/post/44164796353/episode-one", "//span[@class='next_post']//@href", "//div[@class='photo']//img/@src"),
+    "GuildedAge": ("http://guildedage.net/comic/chapter-1-cover/", "//a[@class='navi comic-nav-next navi-next']/@href", "//div[@id='comic']//img/@src")
 }
 
 @click.group()
