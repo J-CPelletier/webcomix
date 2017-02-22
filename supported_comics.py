e2dfea97ac1b7c911d36c99dd84472e5abe57bee
pelletierj97 <pelletierj97@gmail.com>
2017-02-22 08:43:33 -0500

    Put the supported comics in a separated file

---
+++ supported_comics.py
@@ -0,0 +1,9 @@
+supported_comics = {
+    "xkcd": ("http://xkcd.com/1/", "//a[@rel='next']/@href", "//div[@id='comic']//img/@src"),
+    "Nedroid": ("http://nedroid.com/2005/09/2210-whee/", "//div[@class='nav-next']/a/@href", "//div[@id='comic']/img/@src"),
+    "JL8": ("http://limbero.org/jl8/1", "//b[2]/a/@href", "//img/@src"),
+    "SMBC": ("http://www.smbc-comics.com/comic/2002-09-05", "//a[@class='next']/@href", "//img[@id='cc-comic']/@src"),
+    "Blindsprings": ("http://www.blindsprings.com/comic/blindsprings-cover-book-one", "//a[@class='next']/@href", "//img[@id='cc-comic']/@src"),
+    "TheAbominableCharlesChristopher": ("http://abominable.cc/post/44164796353/episode-one", "//span[@class='next_post']//@href", "//div[@class='photo']//img/@src"),
+    "GuildedAge": ("http://guildedage.net/comic/chapter-1-cover/", "//a[@class='navi comic-nav-next navi-next']/@href", "//div[@id='comic']//img/@src")
+}
