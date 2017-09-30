a61a9ab1182eedefcf594f1966272d04f66f41da
Jean-Christophe <pelletierj97@gmail.com>
2017-09-30 15:57:42 -0400

    Add tests for search function

---
+++ webcomictocbz/tests/test_search.py
@@ -0,0 +1,13 @@
+from webcomictocbz.search import search
+from webcomictocbz.comic import Comic
+
+def test_search_searchable_website():
+    searchable_website = search("https://xkcd.com/1/")
+    assert searchable_website.url == "https://xkcd.com/1/"
+    assert searchable_website.next_page_selector == "//*[@*[contains(., '{}')]]//@href".format("next")
+    assert searchable_website.comic_image_selector == "//*[@*[contains(., '{}')]]//@src".format("comic")
+
+def test_search_unsearchable_website():
+    unsearchable_website = search("https://j-cpelletier.github.io/WebComicToCBZ/1.html")
+    assert unsearchable_website == None
+
