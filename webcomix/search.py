86fc4dcb22347f46ea371fa85dd2b03f4cf4e1cc
Jean-Christophe <pelletierj97@gmail.com>
2017-10-26 15:06:11 -0400

    Reformat search function

--- webcomix/search.py
+++ webcomix/search.py
@@ -1,24 +1,21 @@
-import os
-from urllib.parse import urljoin
-from zipfile import ZipFile
-
 import click
-import requests
-from lxml import html
 
 from webcomix.comic import Comic
 
 possible_next_page_xpath = ["next", "Next"]
 possible_image_xpath = ["comic", "Comic"]
 
+
 def discovery(url):
     click.echo("Looking for a path to the whole comic...")
     for next_page in possible_next_page_xpath:
         for image in possible_image_xpath:
-            next_page_xpath = "//*[@*[contains(., '{}')]]//@href".format(next_page)
+            next_page_xpath = "//*[@*[contains(., '{}')]]//@href".format(
+                next_page)
             image_xpath = "//*[@*[contains(., '{}')]]//@src".format(image)
             try:
-                first_pages = Comic.verify_xpath(url, next_page_xpath, image_xpath)
+                first_pages = Comic.verify_xpath(url, next_page_xpath,
+                                                 image_xpath)
                 assert len(set(first_pages)) == 3
                 return Comic(url, next_page_xpath, image_xpath)
             except:
