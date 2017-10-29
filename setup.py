794dcdd6bfe4833b33ee2fdb4cfd40ab477a4963
Jean-Christophe <pelletierj97@gmail.com>
2017-10-29 17:11:35 -0400

    Prepare for deployment on PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,23 @@ from setuptools import setup
 
 setup(
     name='webcomix',
-    version=1.1,
+    version=1.2,
+    description='Webcomic downloader',
+    long_description='webcomix is a webcomic downloader that can additionally create a .cbz file onnce downloaded.',
+    url='https://github.com/J-CPelletier/webcomix',
+    author='Jean-Christophe Pelletier',
+    author_email='pelletierj97@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Internet :: WWW/HTTP',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+    ],
     packages=[
         "webcomix"
     ],
@@ -11,6 +27,7 @@ setup(
         'lxml',
         'requests'
     ],
+    python_requires='>=3.5',
     entry_points='''
         [console_scripts]
         webcomix=webcomix.main:cli
