c2eccb4ce1259830dd641d19624358af83c09549
Jean-Christophe <pelletierj97@gmail.com>
2018-07-30 22:03:18 -0400

    Copy logging from previous version and only yield item to pipeline if a comic image was found

--- webcomix/comic_spider.py
+++ webcomix/comic_spider.py
@@ -1,10 +1,11 @@
 from urllib.parse import urljoin
 
+import click
 import scrapy
 
 
 class ComicSpider(scrapy.Spider):
-    name = "My spider"
+    name = "Comic Spider"
 
     def __init__(self, *args, **kwargs):
         self.start_urls = kwargs.get('start_urls') or []
@@ -13,14 +14,18 @@ class ComicSpider(scrapy.Spider):
         super(ComicSpider, self).__init__(*args, **kwargs)
 
     def parse(self, response):
+        click.echo("Downloading page {}".format(response.url))
         comic_image_url = response.xpath(
             self.comic_image_selector).extract_first()
 
         page = response.meta.get('page') or 1
-        yield {
-            "image_element": urljoin(response.url, comic_image_url),
-            "page": page
-        }
+        if comic_image_url is not None:
+            yield {
+                "image_element": urljoin(response.url, comic_image_url),
+                "page": page
+            }
+        else:
+            click.echo("Could not find comic image.")
         next_page_url = response.xpath(self.next_page_selector).extract_first()
         if next_page_url is not None and not next_page_url.endswith('#'):
             yield scrapy.Request(
