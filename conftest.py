a26d7ceee181e66c6ef5e9b57e86a2bf760c6f40
Jean-Christophe <pelletierj97@gmail.com>
2018-08-04 16:17:44 -0400

    Add cli option to run the checking of supported comics while testing

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--check-supported-comics", action="store_true", default=False, help="checks if supported comics XPath are still working"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--check-supported-comics"):
+        return
+    skip_slow = pytest.mark.skip(reason="need --check-supported_comics option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
--- webcomix/tests/test_misc.py
+++ webcomix/tests/test_misc.py
@@ -1,7 +1,10 @@
+import pytest
+
 from webcomix.comic import Comic
 from webcomix.supported_comics import supported_comics
 
 
+@pytest.mark.slow
 def test_supported_comics():
     for comic_name, comic_info in supported_comics.items():
         first_pages = Comic.verify_xpath(*comic_info)
