5640e97047d6abe8eb625f84b307262cdae0e4ab
Jean-Christophe <pelletierj97@gmail.com>
2018-11-22 16:43:04 -0500

    Add fixtures to make each test using filesystem independent of each other

--- webcomix/tests/test_comic.py
+++ webcomix/tests/test_comic.py
@@ -8,6 +8,39 @@ from webcomix.comic import Comic
 from webcomix.supported_comics import supported_comics
 
 
+@pytest.fixture
+def cleanup_test_directories():
+    if os.path.isdir("xkcd"):
+        shutil.rmtree("xkcd")
+    if os.path.isdir("test"):
+        shutil.rmtree("test")
+    yield None
+    if os.path.isdir("xkcd"):
+        shutil.rmtree("xkcd")
+    if os.path.isdir("test"):
+        shutil.rmtree("test")
+
+
+@pytest.fixture
+def fake_downloaded_xkcd_comic():
+    if os.path.isdir("xkcd"):
+        shutil.rmtree("xkcd")
+    comic = Comic(
+        "xkcd",
+        "http://xkcd.com/1/",
+        "//div[@id='comic']/img/@src",
+        "//a[@rel='next']/@href",
+    )
+    os.mkdir("xkcd")
+    for i in range(1, 6):
+        with open("xkcd/{}.txt".format(i), "w") as image_file:
+            image_file.write("testing {}".format(i))
+    yield comic
+    if os.path.isfile("xkcd.cbz"):
+        os.remove("xkcd.cbz")
+    if os.path.isdir("xkcd"):
+        shutil.rmtree("xkcd")
+
 def test_save_image_location():
     assert (
         Comic.save_image_location(
@@ -18,43 +51,21 @@ def test_save_image_location():
     assert Comic.save_image_location("", 1, "bar") == "bar/1"
 
 
-def test_make_cbz():
-    comic = Comic(
-        "xkcd",
-        "http://xkcd.com/1/",
-        "//div[@id='comic']/img/@src",
-        "//a[@rel='next']/@href",
-    )
-    os.mkdir("xkcd")
-    for i in range(1, 6):
-        with open("xkcd/{}.txt".format(i), "w") as image_file:
-            image_file.write("testing {}".format(i))
-    comic.convert_to_cbz()
+def test_make_cbz(fake_downloaded_xkcd_comic):
+    fake_downloaded_xkcd_comic.convert_to_cbz()
     with ZipFile("xkcd.cbz") as cbz_file:
         for i in range(1, 6):
             with cbz_file.open("{}.txt".format(i), "r") as image_file:
                 assert str(image_file.read()).strip("b'") == "testing {}".format(i)
-    os.remove("xkcd.cbz")
 
 
-def test_make_cbz_corrupted_archive(mocker, capfd):
+def test_make_cbz_corrupted_archive(mocker, capfd, fake_downloaded_xkcd_comic):
     corrupted_archive = mocker.patch.object(ZipFile, "testzip", return_value=mocker.ANY)
-    comic = Comic(
-        "xkcd",
-        "http://xkcd.com/1/",
-        "//div[@id='comic']/img/@src",
-        "//a[@rel='next']/@href",
-    )
-    os.mkdir("xkcd")
-    for i in range(1, 6):
-        with open("xkcd/{}.txt".format(i), "w") as image_file:
-            image_file.write("testing {}".format(i))
     with pytest.raises(BadZipFile):
-        comic.convert_to_cbz()
-    os.remove("xkcd.cbz")
+        fake_downloaded_xkcd_comic.convert_to_cbz()
 
 
-def test_download_runs_the_worker(mocker):
+def test_download_runs_the_worker(mocker, cleanup_test_directories):
     mock_crawler_running = mocker.patch(
         "webcomix.scrapy.crawler_worker.CrawlerWorker.start"
     )
@@ -66,10 +77,9 @@ def test_download_runs_the_worker(mocker):
     )
     comic.download()
     assert mock_crawler_running.call_count == 1
-    shutil.rmtree("xkcd")
 
 
-def test_download_saves_the_files():
+def test_download_saves_the_files(cleanup_test_directories):
     comic = Comic(
         "test",
         "https://j-cpelletier.github.io/webcomix/1.html",
@@ -79,10 +89,9 @@ def test_download_saves_the_files():
     comic.download()
     path, dirs, files = next(os.walk("test"))
     assert len(files) == 2
-    shutil.rmtree("test")
 
 
-def test_download_does_not_add_crawlers_in_main_process(mocker):
+def test_download_does_not_add_crawlers_in_main_process(mocker, cleanup_test_directories):
     mock_crawler_running = mocker.patch(
         "webcomix.scrapy.crawler_worker.CrawlerWorker.start"
     )
@@ -95,7 +104,6 @@ def test_download_does_not_add_crawlers_in_main_process(mocker):
     )
     comic.download()
     assert mock_add_to_crawl.call_count == 0
-    shutil.rmtree("test")
 
 
 def test_verify_xpath():
