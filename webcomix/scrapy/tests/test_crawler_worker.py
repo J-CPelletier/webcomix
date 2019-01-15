3ffd045be41d226bcf1b533c3f5abf95a932eac0
Jean-Christophe <pelletierj97@gmail.com>
2019-01-15 17:42:00 -0500

    Remove duplicate test

--- webcomix/scrapy/tests/test_crawler_worker.py
+++ webcomix/scrapy/tests/test_crawler_worker.py
@@ -20,18 +20,3 @@ def test_spider_raising_error_gets_raised_by_crawler_worker(one_webpage_uri):
 
     with pytest.raises(NextLinkNotFound):
         worker.start()
-
-def test_spider_raising_error_gets_raised_by_crawler_worker(one_webpage_uri):
-    settings = {"LOG_ENABLED": False}
-    worker = CrawlerWorker(
-        settings,
-        False,
-        VerificationSpider,
-        start_urls=[one_webpage_uri],
-        next_page_selector="//div/@href",
-        comic_image_selector="//img/@src",
-        number_of_pages_to_check=2,
-    )
-
-    with pytest.raises(NextLinkNotFound):
-        worker.start()
