ed46ee16ed1b8efcee3697d3da909f72b0755a13
Jean-Christophe Pelletier <pelletierj97@gmail.com>
2021-10-10 10:38:47 -0400

    Add test fixture for docker tests

--- webcomix/tests/test_docker.py
+++ webcomix/tests/test_docker.py
@@ -1,20 +1,29 @@
 import docker
+import pytest
 
-from webcomix.docker import DockerManager
+from webcomix.docker import DockerManager, CONTAINER_NAME
 
-def test_no_javascript_spawns_no_container():
+@pytest.fixture
+def cleanup_container(test):
+    yield None
+    client = docker.from_env()
+    for container in client.containers().list():
+        if container.attrs["Config"]["Image"] == CONTAINER_NAME:
+            container.kill()
+
+def test_no_javascript_spawns_no_container(cleanup_container):
     manager = DockerManager(False)
     manager.__enter__()
     manager.client = docker.from_env()
     assert manager._get_container() is None
 
-def test_javascript_spawns_container():
+def test_javascript_spawns_container(cleanup_container):
     manager = DockerManager(True)
     manager.__enter__()
     assert manager._get_container() is not None
     manager.__exit__(None, None, None)
 
-def test_javascript_exit_removes_container():
+def test_javascript_exit_removes_container(cleanup_container):
     manager = DockerManager(True)
     manager.__enter__()
     manager.__exit__(None, None, None)
