d0d1051b08ee4535e6ad8c4f0b7eccc2199e3a9c
Jean-Christophe Pelletier <jean-christophe.pelletier@unicorne.cloud>
2023-07-31 11:32:20 -0400

    Fix selectors

--- webcomix/supported_comics.py
+++ webcomix/supported_comics.py
@@ -15,8 +15,8 @@ supported_comics = {
     "Nedroid": {
         "name": "Nedroid",
         "start_url": "https://nedroid.com/?1",
-        "comic_image_selector": "//div[@id='comic']//img/@src",
-        "next_page_selector": "//a[contains(@class, 'comic-nav-next')]/@href",
+        "comic_image_selector": "//img[@class='comic']/@src",
+        "next_page_selector": "//a[text()='NEXT>']/@href",
     },
     "JL8": {
         "name": "JL8",
