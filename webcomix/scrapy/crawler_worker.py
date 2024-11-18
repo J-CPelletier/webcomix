61b2a4f796427253fc460bbdeb7389fd8d6cd020
Jean-Christophe Pelletier <pelletierj97@gmail.com>
2024-11-17 23:24:12 -0500

    Revert "Add print"
    This reverts commit 7b2c1e1995a515f011b538d78d99ee52ec68a009.

--- webcomix/scrapy/crawler_worker.py
+++ webcomix/scrapy/crawler_worker.py
@@ -40,8 +40,6 @@ class CrawlerWorker(Process):
         while not self.result_queue.empty():
             result.append(self.result_queue.get())
 
-        print(result)
-
         if self.kill_process:
             raise KeyboardInterrupt
         if len(result) == 1 and isinstance(result[0], Exception):
