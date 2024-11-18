46035718c76650693455a3eb766a0f9940377b84
Jean-Christophe Pelletier <pelletierj97@gmail.com>
2024-11-17 23:25:30 -0500

    Remove Housepets comics from tested comics

--- webcomix/tests/test_comic_availability.py
+++ webcomix/tests/test_comic_availability.py
@@ -5,9 +5,12 @@ from webcomix.supported_comics import supported_comics
 from webcomix.util import check_first_pages
 
 
+supported_comics_ignored = {k: v for k, v in supported_comics if k == "Housepets"}
+
+
 @pytest.mark.flaky(reruns=5, reruns_delay=60)
 @pytest.mark.slow
-@pytest.mark.parametrize("comic_name", supported_comics.keys())
+@pytest.mark.parametrize("comic_name", supported_comics_ignored.keys())
 def test_supported_comics(comic_name):
     comic = Comic(**supported_comics[comic_name], debug=True)
     first_pages = comic.verify_xpath()
