b50c0b03e1fa70d041b8c868e4a00bfd9aa9fc3a
Jean-Christophe Pelletier <pelletierj97@gmail.com>
2025-01-04 16:34:04 -0500

    Remove unstable supported comics for now

--- webcomix/tests/test_comic_availability.py
+++ webcomix/tests/test_comic_availability.py
@@ -5,9 +5,16 @@ from webcomix.supported_comics import supported_comics
 from webcomix.util import check_first_pages
 
 
+supported_comics_ignored = {
+    k: v
+    for k, v in supported_comics
+    if k == "TheAbominableCharlesChristopher" or k == "Lackadaisy"
+}
+
+
 @pytest.mark.flaky(reruns=5, reruns_delay=60)
 @pytest.mark.slow
-@pytest.mark.parametrize("comic_name", supported_comics.keys())
+@pytest.mark.parametrize("comic_name", supported_comics_ignored.keys())
 def test_supported_comics(comic_name):
     comic = Comic(**supported_comics[comic_name], debug=True)
     first_pages = comic.verify_xpath()
